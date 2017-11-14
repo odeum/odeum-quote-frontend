fe4cc5f52014f175281ce0b10d54ec76766d9a4f
kasper879 <kasper8791@gmail.com>
2017-11-14 09:21:51 +0100

    workspace delete

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import {
   MenuPanel,
   Menu,
   Tab,
-  Workspace,
   Footer
 } from 'odeum-app'
 import CreateNewQuote from './Components/createNewQuote';
@@ -19,24 +18,16 @@ class App extends Component {
         <MenuPanel>
           <Menu label={'Tilbud'} icon={'home'} route={'/home'}>
             <Tab icon={'assignment'} label={'Opret Tilbud'}>
-              <Workspace >
                 <CreateNewQuote/>
-          </Workspace>
             </Tab>
             <Tab icon={'assignment'} label={'Tilbuds oversigt'}>
-              <Workspace >
                 Oversigt over alle tilbud
-          </Workspace>
             </Tab>
             <Tab icon={'assignment'} label={'Kunde oversigt'}>
-              <Workspace >
                 Oversigt over alle Kunder
-          </Workspace>
             </Tab>
             <Tab icon={'assignment'} label={'Produkt oversigt'}>
-              <Workspace >
                 Oversigt over alle produkter 
-          </Workspace>
             </Tab>
           </Menu>
         </MenuPanel>
