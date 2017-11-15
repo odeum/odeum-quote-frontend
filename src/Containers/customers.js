80dff6116cde08bda92b361780b2d58a31b79ed4
Frida Jakobsen <1054351@ucn.dk>
2017-11-15 14:52:10 +0100

    customerTable

--- src/Containers/customers.js
+++ src/Containers/customers.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 //import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux'; 
 import { fetchCustomers } from '../Actions/customerAction';
+import { TableWrapper, Table, TD, TH, TR } from '../Styles/createNewQuote'
 
 class Customer extends Component {
     componentDidMount(){
@@ -11,14 +12,31 @@ class Customer extends Component {
     render() {
         const renderCustomer = this.props.customer.map((array, index) => {
             //console.log(array)
-            return Object.entries(array).map((item, key) => {
+            return Object.entries(array).map((item, index) => {
                 console.log(item)
-                return (<h3>{item[1].orgName}</h3>)
+                return (/*<h3 key={key}>{item[1].orgName, }</h3>*/
+                    <TR key={index}>
+                        <TD>{item[1].orgName}</TD>
+                        <TD>{item[1].contactEmail}</TD>
+                        <TD>{item[1].contactPhone}</TD>
+                    </TR>
+                )
             })
         })
         return (
             <div>
-                {renderCustomer}
+                <TableWrapper>
+                    <Table>
+                        <tbody>
+                            <TR style={{backgroundColor: '#E3E5E5'}}>
+                                <TH>Virksomhed</TH>
+                                <TH>E-mail</TH>
+                                <TH>Telefon</TH>
+                            </TR>
+                                {renderCustomer}
+                        </tbody>
+                    </Table>
+                </TableWrapper>
             </div>
         )
     }
