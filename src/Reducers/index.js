bfc6999fad0ebaaaff9cfe8d5a2c35d068af0f3a
kasper879 <kasper8791@gmail.com>
2017-11-16 13:06:35 +0100

    delete Cart and Sheif

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,10 +1,9 @@
-import cart from './cart'; 
+
 import product from './productReducer'
 import customer from './customerReducer'
 import { combineReducers } from 'redux'; 
 
 const rootReducer = combineReducers({
-    cart: cart,
     product: product,
     customer: customer
 }); 
