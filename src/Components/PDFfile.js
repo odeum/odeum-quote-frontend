c62097da30c70fb675e2ee261a2d723543dada7f
fruja <frida.jakobsen@hotmail.com>
2017-11-30 10:15:38 +0100

    pdf

--- src/Components/PDFfile.js
+++ src/Components/PDFfile.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react'
 import Product from '../Containers/products'
 
 class PDFfile extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      person: 'Brian '
+    }
+  }
   render() {
     return (
       <div>
         <h1>Tilbud</h1>
+        <h2>{this.state.person}</h2>
         <p>Dette er en beskrivelse</p>
       </div>
     )
