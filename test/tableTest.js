74d7e935915baf26a7725d6d58f548ec83a2118f
Kasper <kasper8791@gmail.com>
2018-01-01 03:29:01 +0100

    test

--- test/tableTest.js
+++ test/tableTest.js
@@ -1,15 +1,36 @@
-import Table from '../src/Components/table';
+import PopUpWindow from '../src/Components/popupWindow'
+import Button2 from './button';
 import expect from 'expect';
-import { shallow } from 'enzyme';
+import Enzyme, { shallow, mount } from 'enzyme';
 import React from 'react';
+import Adapter from 'enzyme-adapter-react-16';
+import { Popup, InnerPopup, Text, StyledModalHeader, StyledModalHeaderIcon, StyledModalHeaderClose, StyledModalHeaderTitle } from '../src/Styles/popup'; 
+import { ButtonPanel, Button, Icon } from 'odeum-ui'
 
-describe('<Table />', () => {
-  it('renders a <table>', () => {
-    const renderedComponent = shallow(
-      <Table></Table>
-    );
-    expect(
-      renderedComponent.find("tbody").node
-    ).toExist();
+import 'jsdom-global/register';
+
+Enzyme.configure({ adapter: new Adapter() });
+describe('Table Component', () => {
+  it('render table component', () => {
+    const wrapper = shallow(<PopUpWindow />);
+    expect(wrapper).toBe(wrapper);
+  });
+  it('test of it contains of H1', () => {
+    const wrapper = shallow(<PopUpWindow />);
+    expect(wrapper.contains(<h1>hello</h1>)).toEqual(true);
+  });
+  it('Testing StyledCompontents', () => {
+    const wrapper = shallow(<PopUpWindow />);
+    expect(wrapper.contains(<Text>Tilbud</Text>)).toEqual(true);
+  });
+  it('Testing styledComponents with props', () => {
+    const wrapper = shallow(<PopUpWindow />);
+    expect(wrapper.contains(<Icon icon={'check_circle'} iconSize={20} color={'#fff'} active={true} />)).toBe(true);
+  });
+  it('Testing styledComponents with props', () => {
+    const wrapper = shallow(<PopUpWindow />);
+    const IconWrapper = <Icon icon={'close'} iconSize={20||21||23} color={'#fff'} active={true} />
+    expect(wrapper.contains(IconWrapper)).toBe(true);
   });
 });
+
